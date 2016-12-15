ec3962eb505a105d7bb8ef70ee93df75f2598e74
Woratham Kh <kabuto.boy@gmail.com>
2016-12-15 13:45:38 +0700

    build proj

---
+++ desktop/src/com/oop/proj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oop.proj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oop.proj.MyOopProj;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyOopProj(), config);
+	}
+}
